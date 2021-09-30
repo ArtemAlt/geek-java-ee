91cf66cbfbcc9d5e96561729746f73394fd414f1
Артем Алтунин <artem.altunin.vit@gmail.com>
2021-09-30 11:19:15 +0300

    Lesson #5

---
+++ first-jsf-app/src/main/java/org/example/persist/BrandRepository.java
@@ -0,0 +1,70 @@
+package org.example.persist;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.SystemException;
+import javax.transaction.Transactional;
+import javax.transaction.UserTransaction;
+import java.util.List;
+import java.util.Optional;
+
+@ApplicationScoped
+@Named
+public class BrandRepository {
+
+    @PersistenceContext(unitName = "ds")
+    private EntityManager em;
+
+    @Resource
+    private UserTransaction ut;
+
+    @PostConstruct
+    public void init() {
+        if (this.count() == 0) {
+            try {
+                ut.begin();
+                this.save(new Brand(null, "Brand 1"));
+                this.save(new Brand(null, "Brand 2"));
+                this.save(new Brand(null, "Brand 3"));
+                ut.commit();
+            } catch (Exception ex) {
+                try {
+                    ut.rollback();
+                } catch (SystemException exx) {
+                    throw new RuntimeException(exx);
+                }
+            }
+        }
+    }
+
+    public List<Brand> findAll() {
+        return em.createQuery("from Brand ", Brand.class)
+                .getResultList();
+    }
+
+    public Optional<Brand> findById(long id) {
+        return Optional.ofNullable(em.find(Brand.class, id));
+    }
+
+    public Brand getReference(Long id) {
+        return em.getReference(Brand.class, id);
+    }
+
+    @Transactional
+    public Brand save(Brand brand) {
+        if (brand.getId() == null) {
+            em.persist(brand);
+            return brand;
+        }
+        return em.merge(brand);
+    }
+
+    public long count() {
+        return em.createQuery("select count(*) from Brand ", Long.class)
+                .getSingleResult();
+    }
+}
---
+++ first-jsf-app/src/main/java/org/example/persist/CategoryRepository.java
@@ -0,0 +1,70 @@
+package org.example.persist;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.SystemException;
+import javax.transaction.Transactional;
+import javax.transaction.UserTransaction;
+import java.util.List;
+import java.util.Optional;
+
+@ApplicationScoped
+@Named
+public class CategoryRepository {
+
+    @PersistenceContext(unitName = "ds")
+    private EntityManager em;
+
+    @Resource
+    private UserTransaction ut;
+
+    @PostConstruct
+    public void init() {
+        if (this.count() == 0) {
+            try {
+                ut.begin();
+                this.save(new Category(null, "Category 1"));
+                this.save(new Category(null, "Category 2"));
+                this.save(new Category(null, "Category 3"));
+                ut.commit();
+            } catch (Exception ex) {
+                try {
+                    ut.rollback();
+                } catch (SystemException exx) {
+                    throw new RuntimeException(exx);
+                }
+            }
+        }
+    }
+
+    public List<Category> findAll() {
+        return em.createQuery("from Category ", Category.class)
+                .getResultList();
+    }
+
+    public Optional<Product> findById(long id) {
+        return Optional.ofNullable(em.find(Product.class, id));
+    }
+
+    public Category getReference(Long id) {
+        return em.getReference(Category.class, id);
+    }
+
+    @Transactional
+    public Category save(Category category) {
+        if (category.getId() == null) {
+            em.persist(category);
+            return category;
+        }
+        return em.merge(category);
+    }
+
+    public long count() {
+        return em.createQuery("select count(*) from Category ", Long.class)
+                .getSingleResult();
+    }
+}
